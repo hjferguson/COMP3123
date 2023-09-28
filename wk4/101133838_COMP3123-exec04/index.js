59dd8557c0d1e4673a14ec7c6267fee3ff126838
Harlan Ferguson <harlanjazz@hotmail.com>
2023-09-28 19:26:55 -0400

    updates code from lecture

--- wk4/101133838_COMP3123-exec04/index.js
+++ wk4/101133838_COMP3123-exec04/index.js
@@ -3,6 +3,10 @@ var express = require('express');
 const SERVER_PORT = 8089;
 var app = express();
 
+app.use("/test", express.static("./public"))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get("/", (req, res) => {
     res.status(200).send("<h1>Welcome to Express server</h1>");
     res.end();
@@ -11,6 +15,11 @@ app.get("/", (req, res) => {
 app.get("/home", (req, res) => {
     res.status(200).send("<h1>Home Page</h1>");
     res.end();
+})
+
+app.get("/hello", (req,res) => {
+    res.status(200).send("<h1>Hello Express JS!</h1>")
+    res.end
 })
 
 app.post("/person", (req, res) => {
@@ -50,6 +59,13 @@ app.get("/emp", (req,res) => {
     }
 })
 
+//use postman
+app.post("/faculty", (req,res) => {
+    let data = req.body;
+    console.log(data);
+
+    res.send(data)
+})
 
 var names = ["boston", "lettuce", "driving", "crooner"];
 
